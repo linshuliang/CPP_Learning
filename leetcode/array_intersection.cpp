a4eb998bfec8f0e81d916cabcce5bf3887079231
David Lin <xiaolin_2292@qq.com>
2021-01-26 16:46:39 +0800

    update

---
+++ leetcode/array_intersection.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2)
+{
+	std::sort(nums1.begin(), nums1.end());
+	std::sort(nums2.begin(), nums2.end());
+
+	int length1 = nums1.size();
+	int length2 = nums2.size();
+
+	int index1 = 0;
+	int index2 = 0;
+
+	std::vector<int> result;
+	while (index1 < length1 && index2 < length2)
+	{
+		
+		if (nums1[index1] == nums2[index2])
+		{
+			if (result.empty() || nums1[index1] != result.back()) result.push_back(nums1[index1]);
+			index1++;
+			index2++;
+		}
+		else if (nums1[index1] < nums2[index2])
+		{
+			index1++;
+		}
+		else
+		{
+			index2++;
+		}
+	}
+	return result;
+}
+
+void main()
+{
+	std::vector<int> nums1 = { 1, 2, 3, 5, 6, 8, 11, 13, 20 };
+	std::vector<int> nums2 = { 7, 9, 11, 19, 20 };
+
+	std::vector<int> same_elements = intersection(nums1, nums2);
+	for (auto it = same_elements.begin(); it != same_elements.end(); it++)
+		std::cout << *it << "  ";
+	std::cout << std::endl;
+}
---
+++ leetcode/array_intersection_hash.cpp
@@ -0,0 +1,44 @@
+﻿/**
+*  如果使用哈希集合存储元素，则可以在 O(1) 的时间内判断一个元素是否在集合中，从而降低时间复杂度。
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>  // 哈希集合
+
+std::vector<int> get_intersection(std::unordered_set<int>& s1, std::unordered_set<int>& s2);
+
+std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2)
+{
+	std::unordered_set<int> s1, s2;
+	for (auto num : nums1) s1.insert(num);
+	for (auto num : nums2) s2.insert(num);
+	return get_intersection(s1, s2);
+}
+
+std::vector<int> get_intersection(std::unordered_set<int>& s1, std::unordered_set<int>& s2)
+{
+	if (s1.size() > s2.size()) return get_intersection(s2, s1);
+
+	std::vector<int> result;
+	for (auto num : s1)
+	{
+		if (s2.count(num))
+		{
+			result.push_back(num);
+		}
+	}
+
+	return result;
+}
+
+void main()
+{
+	std::vector<int> nums1 = { 1, 2, 3, 5, 6, 8, 11, 13, 20 };
+	std::vector<int> nums2 = { 7, 9, 11, 19, 20 };
+
+	std::vector<int> same_elements = intersection(nums1, nums2);
+	for (auto it = same_elements.begin(); it != same_elements.end(); it++)
+		std::cout << *it << "  ";
+	std::cout << std::endl;
+}
